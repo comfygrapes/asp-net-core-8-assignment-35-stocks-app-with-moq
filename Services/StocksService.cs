86ef2a1a6e3585b6c414ba7298def17e622792b7
Vince Russell Recentes <76193542+comfygrapes@users.noreply.github.com>
2024-03-05 16:27:11 +0800

    Initial commit

---
+++ Services/StocksService.cs
@@ -0,0 +1,57 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTOs;
+using Services.Helpers;
+
+namespace Services
+{
+    public class StocksService : IStocksService
+    {
+        private readonly StockMarketDbContext _stockMarketDbContext;
+
+        public StocksService(StockMarketDbContext stockMarketDbContext)
+        {
+            _stockMarketDbContext = stockMarketDbContext;
+        }
+
+        public async Task<BuyOrderResponse> CreateBuyOrder(BuyOrderRequest? buyOrderRequest)
+        {
+            if (buyOrderRequest == null) throw new ArgumentNullException(nameof(buyOrderRequest));
+            ValidationHelper.ValidateModel(buyOrderRequest);
+
+            var buyOrder = buyOrderRequest.ToBuyOrder(Guid.NewGuid());
+
+            await _stockMarketDbContext.AddAsync(buyOrder);
+            await _stockMarketDbContext.SaveChangesAsync();
+
+            return buyOrder.ToBuyOrderResponse();
+        }
+
+        public async Task<SellOrderResponse> CreateSellOrder(SellOrderRequest? sellOrderRequest)
+        {
+            if (sellOrderRequest == null) throw new ArgumentNullException(nameof(sellOrderRequest));
+            ValidationHelper.ValidateModel(sellOrderRequest);
+
+            var sellOrder = sellOrderRequest.ToSellOrder(Guid.NewGuid());
+
+            await _stockMarketDbContext.AddAsync(sellOrder);
+            await _stockMarketDbContext.SaveChangesAsync();
+
+            return sellOrder.ToSellOrderResponse();
+        }
+
+        public async Task<List<BuyOrderResponse>> GetAllBuyOrders()
+        {
+            var allBuyOrders = _stockMarketDbContext.BuyOrders.Select(buyOrder => buyOrder.ToBuyOrderResponse()).ToList();
+
+            return allBuyOrders;
+        }
+
+        public async Task<List<SellOrderResponse>> GetAllSellOrders()
+        {
+            var allSellOrders = _stockMarketDbContext.SellOrders.Select(sellOrder => sellOrder.ToSellOrderResponse()).ToList();
+
+            return allSellOrders;
+        }
+    }
+}
---
+++ StocksApp/Controllers/TradeController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using StocksApp.Options;
+using ServiceContracts;
+using StocksApp.ViewModels;
+using ServiceContracts.DTOs;
+using Rotativa.AspNetCore;
+
+namespace StocksApp.Controllers
+{
+    [Route("[Controller]")]
+    public class TradeController : Controller
+    {
+        private readonly IFinnhubService _finnhubService;
+        private readonly IOptions<TradingOptions> _tradingOptions;
+        private readonly IConfiguration _configuration;
+        private readonly IStocksService _stocksService;
+
+        public TradeController(IFinnhubService myService, IOptions<TradingOptions> tradingOptions, IConfiguration configuration, IStocksService stocksService)
+        {
+            _finnhubService = myService;
+            _tradingOptions = tradingOptions;
+            _configuration = configuration;
+            _stocksService = stocksService;
+        }
+
+        [HttpGet]
+        [Route("/")]
+        [Route("[Action]")]
+        public async Task<IActionResult> Index()
+        {
+            if (_tradingOptions.Value.DefaultStockSymbol == null)
+            {
+                _tradingOptions.Value.DefaultStockSymbol = "MSFT";
+            }
+            
+            var stock = await _finnhubService.GetStockPriceQuote(_tradingOptions.Value.DefaultStockSymbol);
+            var company = await _finnhubService.GetCompanyProfile(_tradingOptions.Value.DefaultStockSymbol);
+
+            StockTrade stockTrade = new StockTrade()
+            {
+                StockSymbol = _tradingOptions.Value.DefaultStockSymbol,
+                StockName = company?["name"].ToString(),
+                Price = Convert.ToDouble(stock?["c"].ToString()),
+                Quantity = 100,
+            };
+
+            ViewData["ApiKey"] = _configuration["FinnhubToken"];
+            return View(stockTrade);
+        }
+
+        [HttpGet]
+        [Route("[Action]")]
+        public async Task<IActionResult> Orders()
+        {
+            var buyOrders = await _stocksService.GetAllBuyOrders();
+            var sellOrders = await _stocksService.GetAllSellOrders();
+
+            var orders = new Orders()
+            {
+                BuyOrders = buyOrders,
+                SellOrders = sellOrders,
+            };
+
+            return View(orders);
+        }
+
+        [HttpGet]
+        [Route("[Action]")]
+        public async Task<IActionResult> OrdersPDF()
+        {
+            var buyOrders = await _stocksService.GetAllBuyOrders();
+            var sellOrders = await _stocksService.GetAllSellOrders();
+
+            var orders = ((IEnumerable<IOrderResponse>)buyOrders).Concat(sellOrders).OrderByDescending(order => order.DateAndTimeOfOrder).ToList();
+
+            return new ViewAsPdf("OrdersPDF", orders)
+            {
+                PageOrientation = Rotativa.AspNetCore.Options.Orientation.Landscape,
+                PageMargins = new Rotativa.AspNetCore.Options.Margins() { Top = 20, Bottom = 20, Left = 20, Right = 20 },
+            };
+        }
+
+        [HttpPost]
+        [Route("[Action]")]
+        public async Task<IActionResult> BuyOrder(BuyOrderRequest buyOrderRequest)
+        {
+            ModelState.Remove("DateAndTimeOfOrder");
+            if (ModelState.IsValid)
+            {
+                buyOrderRequest.DateAndTimeOfOrder = DateTime.Now;
+
+                await _stocksService.CreateBuyOrder(buyOrderRequest);
+
+                return RedirectToAction("Orders", "Trade");
+            }
+
+            return RedirectToAction("Index", "Trade");
+        }
+
+
+        [HttpPost]
+        [Route("[Action]")]
+        public async Task<IActionResult> SellOrder(SellOrderRequest sellOrderRequest)
+        {
+            ModelState.Remove("DateAndTimeOfOrder");
+            if (ModelState.IsValid)
+            {
+                sellOrderRequest.DateAndTimeOfOrder = DateTime.Now;
+
+                await _stocksService.CreateSellOrder(sellOrderRequest);
+
+                return RedirectToAction("Orders", "Trade"); 
+            }
+
+            return RedirectToAction("Index", "Trade");
+        }
+    }
+}
